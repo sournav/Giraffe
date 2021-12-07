754ef9bbc1d1982508f9680834b736a1f0f78930
Sournav Sekhar Bhattacharya <sournav@tamu.edu>
2021-12-07 11:33:42 -0600

    function comments weighted_graph.zig

--- src/weighted_graph.zig
+++ src/weighted_graph.zig
@@ -17,6 +17,7 @@ pub fn WeightedGraph (comptime index_type: type, comptime weight_type: type, dir
         graph: graph_type(index_type, dir),
         edge_weights: AutoArrayHashMap(index_type, weight_type),
         allocator: *mem.Allocator,
+
         pub fn init(alloc_in: *mem.Allocator) Self {
             return Self {
                 .graph = graph_type(index_type, dir).init(alloc_in),
@@ -29,14 +30,20 @@ pub fn WeightedGraph (comptime index_type: type, comptime weight_type: type, dir
             try self.graph.deinit();
             self.edge_weights.deinit();
         }
+
+        //Adds a node to the graph struct given an index of a node
         pub fn addNode(self: *Self, id: index_type) !void {
             return self.graph.addNode(id);  
         }
+
+        //Adds an edge given the nodes to connect between, and a weight. (Note: for directed graphs, the order of the nodes matter)
         pub fn addEdge(self: *Self, id: index_type, n1_id: index_type, n2_id: index_type, w: weight_type) !void {
             try self.graph.addEdge(id,n1_id,n2_id);
             try self.edge_weights.put(id,w);
 
         }
+
+        //Given an node ID, removes all nodes, an automatically removes all edges connected to it
         pub fn removeNodeWithEdges(self: *Self, id: index_type) !ArrayList(index_type) {
            var removed_edges = try self.graph.removeNodeWithEdges(id);
             for (removed_edges.items) |edge| {
@@ -44,6 +51,8 @@ pub fn WeightedGraph (comptime index_type: type, comptime weight_type: type, dir
             }
             return removed_edges;
         }
+
+        //Removes the edges between two nodes (Note: for directed graphs the order of nodes matter, only edges from n1_id to n2_id will be removed )
         pub fn removeEdgesBetween(self: *Self, n1_id: index_type, n2_id: index_type) !ArrayList(index_type) {
             var edges_removed = try self.graph.removeEdgesBetween(n1_id,n2_id);
             for (edges_removed.items) |index| {
@@ -51,10 +60,14 @@ pub fn WeightedGraph (comptime index_type: type, comptime weight_type: type, dir
             }  
             return edges_removed;
         }
+
+        //Removes the edge by its ID using swapRemove
         pub fn removeEdgeByID(self: *Self, id: index_type) !void {
             try self.graph.removeEdgeByID(id);
             _ = self.edge_weights.swapRemove(id);
         }
+
+        //Gets the weight of an edge if it exists given the index of the edge
         pub fn getEdgeWeight(self: *Self, id: index_type) !weight_type {
             if (!self.edge_weights.contains(id)) {
                 return graph_err.EdgesDoNotExist;
